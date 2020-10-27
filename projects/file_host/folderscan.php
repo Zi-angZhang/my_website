66ca80f5e0e73ae3ba4a2fa5494117c9cf8c3475
ziang zhang <ziang.zhang@kaust.edu.sa>
2020-10-27 14:58:21 +0300

    3d stl viewed added to the file hoster

--- projects/file_host/folderscan.php
+++ projects/file_host/folderscan.php
@@ -1,10 +1,25 @@
 <?php
-  if ($handle = opendir('contents')) {
+function endsWith($string, $endString) 
+{ 
+    $len = strlen($endString); 
+    if ($len == 0) { 
+        return true; 
+    } 
+    return (substr($string, -$len) === $endString); 
+} 
+
+$i = 1;
+if ($handle = opendir('contents')) {
     while (false !== ($file = readdir($handle))) {
-      if ($file != "." && $file != "..") {
-        $thelist .= '<li><a href="contents/'.$file.'">'.$file.'</a></li>';
-      }
+        if ($file != "." && $file != "..") {
+            $thelist .= '<li><a href="contents/'.$file.'">'.$file.'</a></li>';
+        }
+        if (endsWith($file, 'stl')){
+            $stlTable .= '<tr><th><dir id=stlViewer'.$i.'></dir></th><th><li><a href="contents/'.$file.'">'.$file.'</a></li></th></tr>';
+            $stlViewer .= 'var stl_viewer'.$i.'=new StlViewer(document.getElementById("stlViewer'.$i.'"), {models: [{id:0, filename:"contents/'.$file.'"}]});';
+            $i += 1;
+        }
     }
-    closedir($handle);
-  }
+closedir($handle);
+}
 ?>
--- projects/file_host/index.php
+++ projects/file_host/index.php
@@ -1,18 +1,40 @@
 <!DOCTYPE html>
 <html>
+<head>
+<script type="text/javascript" src='stl_viewer.min.js'></script>
+<style type="text/css">
+table, th, td {
+border: 1px solid black;
+}
+</style>
+</head>
 <body>
     <h1>hosted files</h1>
 <?php include 'folderscan.php';?>
-
 <?php
     echo $thelist;
-    
 ?>
 <h3>upload</h3>
 <form action="upload.php" method="post" enctype="multipart/form-data">
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload" name="submit">
 </form>
+<h2>3d object viewer</h2>
+<table style=width:90%>
+<tr>
+    <th>model</th>
+    <th>link</th>
+</tr>
+<?php
+    echo $stlTable;
+?>
+</table>
+<div id='stl_cont'></div>
+    <script type="text/javascript">
+    <?php
+        echo $stlViewer
+    ?>
+    </script>
 </body>
 </html>
 
